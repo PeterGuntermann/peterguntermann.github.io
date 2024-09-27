3fa18dc1a6e23b2ac6e7f892aa8ceade0ac6d604
Peter Guntermann <pgu@posteo.de>
2024-09-27 22:36:13 +0200

    display rough coversongs table

---
+++ pages/coversongs.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+type Coversong = {
+  title: string;
+  artist: string;
+};
+
+const songs: Coversong[] = [
+  { title: 'Titel A', artist: 'Interpret A' },
+  { title: 'Titel B', artist: 'Interpret B' },
+  { title: 'Titel C', artist: 'Interpret C' },
+];
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <h2>Coversongs</h2>
+
+    <table class="w-full table-auto">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="song of songs">
+          <td>
+            {{ song.title }}
+          </td>
+          <td>
+            {{ song.title }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [
+    //
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
 };
