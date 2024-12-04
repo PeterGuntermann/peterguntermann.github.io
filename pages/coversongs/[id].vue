e94cc9d149d205653b9a54c52ff47d34fc6b2ce8
Peter Guntermann <pgu@posteo.de>
2024-12-04 22:34:03 +0100

    Render coversong lyrics page from markdown

--- pages/coversongs/[id].vue
+++ pages/coversongs/[id].vue
@@ -1,19 +1,15 @@
-<script setup lang="ts">
-const route = useRoute();
-
-console.log(route.params.id);
-const sheet = await queryContent('/coversongs/' + route.params.id).findOne();
-console.log(sheet);
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <NuxtLink :to="`/coversongs`"> Zurück </NuxtLink>
-  <pre>
-ID    : {{ route.params.id }}
-Artist: {{ sheet.artist }}
-Key   : {{ sheet.key }}
-Body  : {{ JSON.stringify(sheet.body) }} # TODO: parse and display
-  </pre>
-</template>
+  <NuxtLink class="btn btn-outline btn-info btn-sm" :to="`/coversongs`">
+    ← Zurück
+  </NuxtLink>
 
-<style scoped></style>
+  <ContentDoc v-slot="{ doc }">
+    <article class="container mx-auto px-4 prose">
+      <h1>{{ doc.title }}</h1>
+      <em>by {{ doc.artist }}</em>
+      <ContentRenderer :value="doc" />
+    </article>
+  </ContentDoc>
+</template>
