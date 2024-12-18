649e5faeb87a9925c19ddee07101d0b0f33556f2
Peter Guntermann <pgu@posteo.de>
2024-12-18 19:37:46 +0100

    extract component for the HTML table
    # Conflicts:
    #	pages/coversongs/index.vue

---
+++ components/coversongs-html-table.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Coversong } from '~/models/coversong';
+
+const { coversongs } = defineProps<{
+  coversongs: Coversong[];
+}>();
+</script>
+
+<template>
+  <div class="overflow-y-auto">
+    <table class="table table-sm table-zebra w-full table-auto">
+      <thead>
+        <tr class="text-left">
+          <th>Title</th>
+          <th>Artist</th>
+          <th>Sheet</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="song of coversongs">
+          <td>
+            {{ song.title }}
+          </td>
+          <td>
+            {{ song.artist }}
+          </td>
+          <td>
+            <NuxtLink v-if="song.id" :to="`/coversongs/${song.id}`">
+              <span
+                v-if="song.status === 'ready'"
+                class="btn btn-xs btn-outline btn-success"
+              >
+                Sheet Ready
+              </span>
+              <span
+                v-else-if="song.status === 'draft'"
+                class="btn btn-xs btn-outline btn-warning"
+              >
+                Sheet Draft
+              </span>
+              <span v-else class="btn btn-xs btn-outline"> Sheet Todo </span>
+            </NuxtLink>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
--- pages/coversongs/index.vue
+++ pages/coversongs/index.vue
@@ -53,44 +53,7 @@ const percent = (num: number) => ((num / numSongs) * 100).toFixed(0);
       </div>
     </nav>
 
-    <div class="overflow-y-auto">
-      <table class="table table-sm table-zebra w-full table-auto">
-        <thead>
-          <tr class="text-left">
-            <th>Title</th>
-            <th>Artist</th>
-            <th>Sheet</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="song of songs">
-            <td>
-              {{ song.title }}
-            </td>
-            <td>
-              {{ song.artist }}
-            </td>
-            <td>
-              <NuxtLink v-if="song.id" :to="`/coversongs/${song.id}`">
-                <span
-                  v-if="song.status === 'ready'"
-                  class="btn btn-xs btn-outline btn-success"
-                >
-                  Sheet Ready
-                </span>
-                <span
-                  v-else-if="song.status === 'draft'"
-                  class="btn btn-xs btn-outline btn-warning"
-                >
-                  Sheet Draft
-                </span>
-                <span v-else class="btn btn-xs btn-outline"> Sheet Todo </span>
-              </NuxtLink>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <CoversongsHtmlTable :coversongs="songs" />
   </div>
 </template>
 
