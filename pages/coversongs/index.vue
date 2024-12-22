c4d78c0fcbafbb53b3e9771f2132733848c7ef00
Peter Guntermann <pgu@posteo.de>
2024-12-22 10:58:33 +0100

    delete unused code

--- pages/coversongs/index.vue
+++ pages/coversongs/index.vue
@@ -1,25 +1,5 @@
 <script setup lang="ts">
-import { getCoversongs } from '~/composables/coversongs';
 import { Coversong } from '~/models/coversong';
-
-let stop1 = new Date();
-let xxx = [
-  ...(await getCoversongs()),
-  ...(await getCoversongs()),
-  ...(await getCoversongs()),
-  ...(await getCoversongs()),
-  ...(await getCoversongs()),
-];
-let stop2 = new Date();
-console.log(`Fetch dauerte ${(stop2 - stop1) / 1000}s`);
-console.log(xxx);
-let stop3 = new Date();
-console.log(`Display dauerte ${(stop3 - stop2) / 1000}s`);
-
-// const filter = async () => {
-//   xxx = await getCoversongs({ title: { $contains: 'Nacht' } });
-//   console.log('filter', xxx);
-// };
 
 const songs: Coversong[] = await getCoversongsByCsv();
 // const songs: Coversong[] = await getCoversongs();
@@ -37,10 +17,7 @@ const percent = (num: number) => ((num / numSongs) * 100).toFixed(0);
         <h2>Coversongs</h2>
       </div>
 
-      <div class="flex-1">
-        <!-- Debug -->
-        <!--        <button class="btn" @click="filter()">Filter</button>-->
-      </div>
+      <div class="flex-1"></div>
 
       <div class="flex-none">
         <span class="mx-1 badge badge-outline"> {{ numSongs }} songs </span>
