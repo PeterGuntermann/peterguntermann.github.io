6730607e7b1479efcf2a9f438c970f82870631fa
Peter Guntermann <pgu@posteo.de>
2024-12-23 10:42:44 +0100

    register CellRenderer for nuxtlink to detail pages

--- components/coversongs-grid.vue
+++ components/coversongs-grid.vue
@@ -7,8 +7,8 @@ import {
   ModuleRegistry,
   themeQuartz,
 } from 'ag-grid-community';
+import { SheetLinkCellRenderer } from '#components';
 
-// to use myTheme in an application, pass it to the theme grid option
 const theme = themeQuartz.withParams({
   backgroundColor: 'transparent',
   browserColorScheme: 'dark',
@@ -19,25 +19,17 @@ const theme = themeQuartz.withParams({
   oddRowBackgroundColor: 'oklch(var(--b2)/var(--tw-bg-opacity))',
 });
 
-// Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-// Row Data: The data to be displayed.
-const rowData = ref([
-  { title: 'Tesla', artist: 'Model Y', sheet: 64950 },
-  { title: 'Ford', artist: 'F-Series', sheet: 33850 },
-  { title: 'Toyota', artist: 'Corolla', sheet: 29600 },
-]);
-
-// Column Definitions: Defines the columns to be displayed.
-const colDef: ColDef = { flex: 1, resizable: false };
+const colDef: ColDef = { resizable: false };
 const colDefs = ref<ColDef[]>([
-  { ...colDef, field: 'title' },
-  { ...colDef, field: 'artist' },
+  { ...colDef, flex: 1, field: 'title' },
+  { ...colDef, flex: 1, field: 'artist' },
   {
     ...colDef,
     field: 'sheet',
-    valueFormatter: (x) => `Value: ${x.value}`,
+    cellRenderer: 'SheetLinkCellRenderer',
+    cellRendererParams: { foo: 'bar' },
   },
 ]);
 
@@ -45,26 +37,18 @@ const { coversongs } = defineProps<{
   coversongs: Coversong[];
 }>();
 
-const mapData = (coversongs: Coversong[]) =>
-  coversongs.map((x) => ({
-    title: x.title,
-    artist: x.artist,
-    sheet: x.status,
-  }));
+defineExpose({
+  SheetLinkCellRenderer,
+});
 </script>
 
 <template>
   <AgGridVue
     class="table"
-    :rowData="mapData(coversongs)"
+    :rowData="coversongs"
     :columnDefs="colDefs"
     :theme="theme"
     :pagination="true"
     style="width: 100%; height: 500px"
   />
-
-  <pre v-if="false">
-Coversongs:
-{{ JSON.stringify(coversongs, null, 2) }}
-  </pre>
 </template>
