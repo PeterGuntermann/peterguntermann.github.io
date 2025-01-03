44745c177089149cbbd8ea9421e349bb2182a49a
Peter Guntermann <pgu@posteo.de>
2025-01-03 16:31:05 +0100

    add debug

--- pages/songs/coversongs/[id].vue
+++ pages/songs/coversongs/[id].vue
@@ -6,6 +6,14 @@
   </NuxtLink>
 
   <ContentDoc v-slot="{ doc }">
+    <div class="collapse bg-base-200">
+      <input type="checkbox" />
+      <div class="collapse-title">Debug</div>
+      <div class="collapse-content">
+        <pre>{{ JSON.stringify(doc, null, 2) }}</pre>
+      </div>
+    </div>
+
     <article class="container mx-auto px-4 prose">
       <h1>{{ doc.title }}</h1>
       <em>{{ doc.artist }}</em>
